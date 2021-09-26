461ff8ab13587a293236a3b1e37a64f9957358fa
sohel72989 <rana72989@gmail.com>
2021-09-26 23:50:04 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import fakedata from './fakedata';
+import image from './images/download.jpg';
+import { useState } from 'react';
+import '../src/App.css'
+import { faLink } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+  const [totalAmout, setTotalAmout] = useState(0);
+  const [person, setPerson] = useState([]);
+  const handleAddAmount = (name, amount) => {
+    const newPerson = { name, amount };
+    setPerson([...person, newPerson]);
+    setTotalAmout(totalAmout + amount)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div className='text-center App-header mb-3'>
+        <h1>Web Application Project</h1>
+        <p><strong>Total Budget 1000k</strong></p>
+      </div>
+      <div className='row ms-3'>
+        <div className='col-9'>
+          <div className='row'>
+            {
+              fakedata.map(({ id, name, age, experience, role, amount }) => (
+                <div key={id} className="card col-4">
+                  <img src={image} className="card-img-top w-100" alt="web developer" />
+                  <div className="card-body">
+                    <h5 className="card-title">Name: {name} </h5>
+                    <p className="card-text"> <strong> Role: {role} </strong> </p>
+                    <p className="card-text"> <strong> Amount: {amount}k </strong> </p>
+                    <p className="card-text"> <strong> Experience: {experience} years </strong> </p>
+                    <p className="card-text"> <strong> Age: {age} years </strong> </p>
+                    <button onClick={() => handleAddAmount(name, amount)} className="button-style"><i className="fa fa-save"></i> Add To Team</button>
+                    <div className=" App-logo">
+                      <a href="https://www.facebook.com/rana72989/"><i className="fa fa-facebook"></i></a>
+                    </div>
+
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+        <div className='col-sm-3'>
+          <div className='row'>
+            {
+              person.map((p, i) => (
+                <div className='d-flex my-2 border' key={i}>
+                  <img src={image} className="w-25" alt="web developer" />
+
+                  <div>
+                    <p>Name: {p.name}</p>
+                    <p>Amount: {p.amount}k</p>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+
+          <h4 className="text-secondary ">Total Amount: {totalAmout}k </h4>
+        </div>
+      </div>
+    </div >
   );
 }
 
---
+++ src/fakedata.js
@@ -0,0 +1,52 @@
+const fakedata = [
+    {
+        name: "Sohel Rana",
+        role: "Backend Developer",
+        amount: 40,
+        id: '1',
+        experience: 2,
+        age: 24,
+    },
+    {
+        name: "Hashim Reza ",
+        role: "fontend web developer",
+        amount: 30,
+        id: '2',
+        experience: 1,
+        age: 23,
+    },
+    {
+        name: "Yeasin Arafat",
+        role: "UI Designer",
+        amount: 30,
+        id: '3',
+        experience: 4,
+        age: 28,
+    },
+    {
+        name: "Rajib Hossain",
+        role: "System Analyzer",
+        amount: 20,
+        id: '4',
+        experience: 3,
+        age: 26,
+    },
+    {
+        name: "Samiul Islam",
+        role: "Architecture",
+        amount: 20,
+        id: '5',
+        experience: 1,
+        age: 25,
+    },
+    {
+        name: "Mizan Sarkar",
+        role: "Project Manager",
+        amount: 20,
+        id: '5',
+        experience: 1,
+        age: 25,
+    },
+]
+
+export default fakedata;
